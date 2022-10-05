926fc8926ee44cdd6160412b358f05988579b64f
Nathaniel Boyd <nathanielboydvillarroel@gmail.com>
2022-10-04 21:34:51 -0300

    SPRINT 3 v1.1

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 const mainRouter = require('./src/routes/mainRoutes')
-
+const port = process.env.PORT || 3000
 const express = require('express');
 const path = require('path');
 const app = express();
+
+// ---------- Motor de plantillas ----------
 app.set('view engine', 'ejs');
+app.set('views', './views');
 
 
-
-app.use(express.static(path.resolve(__dirname, ('./public'))));
+// ---------- Recursos estáticos ----------
+app.use(express.static('public'));
 
 app.use('/', mainRouter)
 
 
-app.listen(process.env.PORT || 3000, function () {
-    console.log('Servidor corriendo en puerto 3000')
+app.listen(port, function () {
+    console.log(`Servidor corriendo en puerto ${port}`)
 });
