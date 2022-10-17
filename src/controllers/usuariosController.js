1c04e7492491efa74148f999a718bb7e2a8ef5cc
aguloco <agusplatero@hotmail.com>
2022-10-17 13:53:32 -0300

    Multer funcional en Registro (se puede subir la foto del perro. La idea sería que se guarde en la sección BLOG?? que todavía no tenemos hecha + Arreglados los botones registro y login dentro de esa sección

--- src/controllers/usuariosController.js
+++ src/controllers/usuariosController.js
@@ -4,6 +4,10 @@ const path = require('path');
 //const usuariosFilePath = path.join(__dirname, '../data/usuarios.json');
 // const tienda = JSON.parse(fs.readFileSync(tiendaFilePath, 'utf-8'));
 
+
+
+// -------------------- CONTROLADOR USUARIOS --------------------
+
 const controller = {
     
     login: (req, res) => {
@@ -14,5 +18,40 @@ const controller = {
         res.render('registro')
     }   
 }
+
+// ----------  ----------
+// Create -  Method to store
+store: (req, res) => {
+	
+    idNuevo=0;
+
+    for (let s of products){
+        if (idNuevo<s.id){
+            idNuevo=s.id;
+        }
+    }
+
+    idNuevo++;
+
+    let nombreImagen = req.file.filename;
+
+    let productoNuevo =  {
+        id:   idNuevo,
+        name: req.body.name ,
+        price: req.body.price,
+        discount: req.body.discount,
+        category: req.body.category,
+        description: req.body.description,
+        image: nombreImagen
+    };
+
+    products.push(productoNuevo);
+
+    fs.writeFileSync(productsFilePath, JSON.stringify(products,null,' '));
+
+    res.redirect('/');
+    
+},
+
 
 module.exports = controller;
--- src/routes/usuariosRouter.js
+++ src/routes/usuariosRouter.js
@@ -6,13 +6,29 @@ const multer = require('multer');
 const usuariosController = require('../controllers/usuariosController');
 
 //***  Configuración de multer  ****/
+const multerDiskStorage = multer.diskStorage({
+    
+    destination: function(req, file, cb) {       
+     cb(null, path.join(__dirname,"../../public/img/usuarios"));   
+    },
+    
+    filename: function(req, file, cb) {          
+     let imageName = Date.now() + file.originalname;  
+     cb(null, imageName);         
+    }
+});
 
+const uploadFile = multer({ storage: multerDiskStorage });
 
 // ********** RUTAS **********
 
 router.get('/login', usuariosController.login);
+router.get('/create', usuariosController.login);
+router.post('/create', uploadFile.single('imageDOG'), usuariosController.login);
 
 router.get('/registro', usuariosController.registro);
+router.get('/create', usuariosController.registro);
+router.post('/create', uploadFile.single('imageDOG'), usuariosController.registro);
 
 
 // ********** Exportación de las rutas. No tocar **********
