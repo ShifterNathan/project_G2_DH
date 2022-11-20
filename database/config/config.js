5600586bb7a711fbf5b2c8d55d3a49c3104697e2
zulema3165 <mzalvarez11@gmail.com>
2022-11-20 16:00:21 +0100

    ultimosCambios

--- database/config/config.js
+++ database/config/config.js
@@ -1,24 +1,26 @@
+require('dotenv').config();
+
 module.exports ={
   "development": {
     "username": "root",
     "password": null,
-    "database": "mydb_db",
-    "host": "127.0.0.1",
+    "database": "doghousefood_",
+    "host": "mysql-doghousefood.alwaysdata.net",
     "dialect": "mysql",
     "port": 3306
   },
   "test": {
     "username": "root",
     "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
+    "database": "doghousefood_test",
+    "host": "mysql-doghousefood.alwaysdata.net",
     "dialect": "mysql"
   },
   "production": {
     "username": "root",
     "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
+    "database": "doghousefood_web",
+    "host": "mysql-doghousefood.alwaysdata.net",
     "dialect": "mysql"
   }
 }
