a74d86c50abcc059beacb869bcca0a0f1a429de8
Nathaniel Boyd <nathanielboydvillarroel@gmail.com>
2022-12-18 12:01:56 -0300

    login-register

--- src/middlewares/validacionesRegistro.js
+++ src/middlewares/validacionesRegistro.js
@@ -6,10 +6,10 @@ const { body } = require('express-validator');
 const path = require('path');
 
 let validacionesRegistro = [
-    body('nombreUsuario').notEmpty().withMessage('Debes completar tu nombre'),
-    body('apellidoUsuario').notEmpty().withMessage('Debes completar tu apellido'),
+    body('nombreUsuario').notEmpty().withMessage('Ingresa tu nombre'),
+    body('apellidoUsuario').notEmpty().withMessage('Ingresa tu apellido'),
     body('emailUsuario')
-        .notEmpty().withMessage('Debes completar tu email').bail()
+        .notEmpty().withMessage('Ingresa tu email').bail()
         .isEmail().withMessage('Formato de email inválido'),
         /*.custom(value => {
             userInvalid = user.findByEmail(value)
@@ -17,14 +17,14 @@ let validacionesRegistro = [
                     return Promise.reject('Este e-mail ya está en uso')
                 }
         }),*/
-    body('claveUsuario').notEmpty().withMessage('Debes completar tu clave'),
-    body('direccionUsuario').notEmpty().withMessage('Debes completar tu dirección'),
+    body('claveUsuario').notEmpty().withMessage('Ingresa una contraseña'),
+    body('direccionUsuario').notEmpty().withMessage('Ingresa tu dirección'),
     body('avatar').custom((value, { req }) => {
         let file = req.file;
         let acceptedExtensions = ['.jpg', '.jpeg', '.png', '.gif'];
             
         if (!file) { //cuando no suben un archivo
-            throw new Error ('Falta subir imagen');
+            throw new Error ('Carga una imágen');
         } else {  //cuando suben un archivo, verificar la extensión del mismo
             let fileExtension = path.extname(file.originalname);
             if (!acceptedExtensions.includes(fileExtension)){
