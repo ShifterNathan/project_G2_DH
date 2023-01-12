8d3b7b51475fb4a923afc5ce384d063b0af675e1
Nathaniel Boyd <nathanielboydvillarroel@gmail.com>
2023-01-11 22:40:23 -0300

    Captura de info para el carrito

--- public/js/cart.js
+++ public/js/cart.js
@@ -35,17 +35,86 @@ onLoadCartNumbers();
 
 
 
+
+
+
+
+
 // -------------------- Captura de info del producto para la vista del carrito --------------------
 
-let addToCart = document.querySelector('#addToCart');
+const addToCart = document.getElementsByClassName('add-cart');
+const productRow = document.getElementsByClassName('product-row');
 
-addToCart.addEventListener('click', (event) => {
+for (let i = 0; i < addToCart.length; i++){
+    button = addToCart[i]
+    button.addEventListener('click', addToCartClicked);
+}
 
-    let  precio= event.target.parentNode.parentNode.firstElementChild.firstElementChild.nextElementSibling.firstElementChild.innerText.substring(1)
-    console.log(precio)
-    console.log(typeof(precio))
-    let descuento = event.target.parentNode.parentNode.firstElementChild.firstElementChild.nextElementSibling.firstElementChild.nextElementSibling.firstElementChild.innerText
-    console.log(descuento)
-    let nombre = event.target.parentNode.parentNode.firstElementChild.firstElementChild.nextElementSibling.firstElementChild.nextElementSibling.nextElementSibling 
-    console.log(nombre)
-});
+function addToCartClicked (event){
+    button = event.target;
+    const cartItem = button.parentNode.parentNode;
+    const name = cartItem.getElementsByClassName('product-name')[0].innerText;
+    const price = cartItem.getElementsByClassName('product-price')[0].innerText;
+    const discount = cartItem.getElementsByClassName('badge')[0].innerText;
+    const imgSrc = cartItem.getElementsByClassName('img-fluid')[0].src;
+    console.log('name')
+    console.log('price')
+    console.log('discount')
+    console.log('imgSrc')
+    addItemToCart(imgSrc, name, price, discount);
+}
+
+function addItemToCart(imgSrc, name, price, discount){
+    const productRow = document.createElement('div');
+    productRow.classList.add('product-row');
+    const productRows = document.getElementsByClassName('product-rows')[0];
+    let cartName = document.getElementsByClassName('cart-name');
+    let cartPrice = document.getElementsByClassName('cart-price');
+    let cartDiscount = document.getElementsByClassName('cart-discount');
+    let cartImage = document.getElementsByClassName('cart-image');
+
+    const cartRowItems = `
+    <div class="">
+        <img class="cart-image" src="${imgSrc}">
+        <p class="cart-name">${name}</p>
+        <p class="cart-price">${price}</p>
+        <p class="cart-discount">${discount}</p>
+        <button class="remove-btn">Remove</button>
+    </div>
+    `
+
+
+    productRow.innerHTML = cartRowItems;
+    productRows.append(productRow);
+}
+
+    
+
+
+
+
+// let addToCart = document.querySelector('#addToCart');
+
+// addToCart.addEventListener('click', (event) => {
+
+//     let product = event.target.parentNode.parentNode;
+//     readProduct(product);
+//     console.log(product);
+
+//     let  precio= event.target.parentNode.parentNode.firstElementChild.firstElementChild.nextElementSibling.firstElementChild.innerText.substring(1)
+//     console.log(precio)
+//     console.log(typeof(precio))
+//     let descuento = event.target.parentNode.parentNode.firstElementChild.firstElementChild.nextElementSibling.firstElementChild.nextElementSibling.firstElementChild.innerText
+//     console.log(descuento)
+//     let nombre = event.target.parentNode.parentNode.firstElementChild.firstElementChild.nextElementSibling.firstElementChild.nextElementSibling.nextElementSibling 
+//     console.log(nombre)
+// });
+
+function readProduct(product){
+    const infoProuct = {
+        nombre: product.querySelector('.product-name p'),
+        precio: product.querySelector('.product-price p'),
+        descuento: product.querySelector('.discount span'),
+        img: product.querySelector('img').src
+    }
+}
