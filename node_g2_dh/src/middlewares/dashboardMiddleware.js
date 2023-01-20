3b12607e8fe1cc333575bf5c1d33aaf8ebf10b16
Nathaniel Boyd <nathanielboydvillarroel@gmail.com>
2023-01-20 04:09:29 -0300

    api sin mw v2

--- node_g2_dh/src/middlewares/dashboardMiddleware.js
+++
@@ -1,8 +0,0 @@
-function dashboardMiddleware (req, res, next) {
-    if(req.session.userLogged.rol == "COMUN") {
-        return res.redirect('/');
-    }
-    next();
-}
-
-module.exports = dashboardMiddleware; 
--- node_g2_dh/src/routes/apiRouter.js
+++ node_g2_dh/src/routes/apiRouter.js
@@ -1,13 +1,12 @@
 const apiController = require('../controllers/apiController');
-
 const express = require('express');
 const router = express.Router();
-const dashboardMiddleware = require('../middlewares/dashboardMiddleware')
 
-router.get('/usuarios', dashboardMiddleware, apiController.usuarios);
-router.get('/usuarios/:id', dashboardMiddleware, apiController.usuario);
 
-router.get('/productos', dashboardMiddleware, apiController.productos);
-router.get('/productos/:id', dashboardMiddleware, apiController.producto);
+router.get('/usuarios', apiController.usuarios);
+router.get('/usuarios/:id', apiController.usuario);
+
+router.get('/productos', apiController.productos);
+router.get('/productos/:id', apiController.producto);
 
 module.exports = router;
